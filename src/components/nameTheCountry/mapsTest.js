b8585dae71571f78716086c3e78fdd0c82ed2731
oscarwarnling <6603922+oscarwarnling@users.noreply.github.com>
2019-04-15 12:55:39 +0200

    Add files via upload
    Latest

---
+++ src/components/nameTheCountry/mapsTest.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../css/style.css"
+import "../css/nameTheFlag.css"
+//import {Link} from "react-router-dom";
+import Map from './map'
+
+const MapsTest = () => {
+    return (
+
+        <div className="container main" >
+            <div>
+                <Map />
+            </div>
+        </div>
+    )
+}
+
+
+export default MapsTest;
