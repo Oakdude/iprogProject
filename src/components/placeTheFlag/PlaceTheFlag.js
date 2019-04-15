9c79fc765e2fab3597d4ca96a965d1745b57bc33
oscarwarnling <6603922+oscarwarnling@users.noreply.github.com>
2019-04-15 22:15:22 +0200

    Add files via upload

---
+++ src/components/placeTheFlag/PlaceTheFlag.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import "../css/style.css"
+import "../css/placeTheFlag.css"
+import MapsTest from '../nameTheCountry/mapsTest'
+import {Link} from "react-router-dom"
+import FetchACountryFlag from './FetchACountryFlag'
+import Timer from "react-compound-timer"
+
+
+const Selection = () => {
+    return (
+        <div className="container main">
+
+            <div className='row returnButton'>
+                <div className="col-md-3">
+                    <Link to='/'>
+                    <button type="button" className="btn btn-danger">Return</button>
+                    </Link>
+                </div>
+                <div id="game2div">
+                <div id="gameTimer">
+
+                <Timer formatValue={(value) => `${(value < 10 ? `${value}` : value)} `}>
+
+    <Timer.Minutes formatValue={(value) => `${(value < 10 ? `0${value}` : value)}:`}/>
+    <Timer.Seconds formatValue={(value) => `${(value < 10 ? `0${value}` : value)}`} />
+
+</Timer></div>
+                <div className="progress">
+                    <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="10"
+                    aria-valuemin="10" aria-valuemax="100">
+                    1/10
+                    </div>
+                </div>
+                  <FetchACountryFlag />
+<MapsTest />
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Selection;
